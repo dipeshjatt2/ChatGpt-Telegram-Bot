dbb0d2e35b20c4637c77814d8ab6bf7b40bf3dfd
dipeshjatt2 <101973614+dipeshjatt2@users.noreply.github.com>
2024-11-18 16:24:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN ='YOUR BOT TOKERN ' # bot token from t.me/botfather
+BOT_TOKEN ='7485383982:AAGZAfY_kiwROsxqKeiVtYBEHcA7vmWGdZM ' # bot token from t.me/botfather
 API_ID = YOUR API ID #api id from telegram.org
 API_HASH = YOUR API HASH #api hash from telegram.org
     
