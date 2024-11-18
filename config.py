8649722707399a11565d5635b49e6109b440e1f5
dipeshjatt2 <101973614+dipeshjatt2@users.noreply.github.com>
2024-11-18 16:28:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
 BOT_TOKEN ='7485383982:AAGZAfY_kiwROsxqKeiVtYBEHcA7vmWGdZM ' # bot token from t.me/botfather
 API_ID = 22118129 #api id from telegram.org
-API_HASH = 43c66e3314921552d9330a4b05b18800  #api hash from telegram.org
+API_HASH = '43c66e3314921552d9330a4b05b18800'  #api hash from telegram.org
     
